83257c0836e1b32d366d617616127036ba01f96f
Ava Pun <37046477+AvaLovelace1@users.noreply.github.com>
2025-11-07 16:18:15 -0500

    Add feedback buttons

--- src/demo/app.py
+++ src/demo/app.py
@@ -1,22 +1,25 @@
 import json
 import os
-import shutil
 import subprocess
 import tempfile
 import time
 import uuid
 from dataclasses import fields
 from pathlib import Path
+from typing import Sequence, Any
 
 import gradio as gr
 import torch
 import transformers
 from brickgpt.models import BrickGPT, BrickGPTConfig
+from gradio import Component
 
 
 class Demo:
     def __init__(self, output_dir: str, model_cfg: BrickGPTConfig):
-        self.generator = BrickGenerator(output_dir, model_cfg)
+        flagging_dir = '/data/apun/brickgpt_demo_out'
+        self.generator = BrickGenerator(output_dir, flagging_dir, model_cfg)
+        self.logger = Logger()
 
         # Define inputs and outputs
         in_prompt = gr.Textbox(label='Input prompt', info='Text prompt for which to generate a brick structure.',
@@ -39,6 +42,7 @@ class Demo:
         out_txt = gr.Textbox(label='Output bricks', lines=5, max_lines=5, show_copy_button=True,
                              info='The brick structure in text format. Each line of the form "hxw (x,y,z)" represents a '
                                   '1-unit-tall rectangular brick with dimensions hxw placed at coordinates (x,y,z).')
+        out_metadata = gr.JSON(label='out_metadata', visible=False)
 
         self.demo = gr.Interface(
             fn=self.generator.generate_bricks,
@@ -50,8 +54,12 @@ class Demo:
                         'If you encounter any problems, [create an issue on GitHub](https://github.com/AvaLovelace1/BrickGPT/issues/new/choose).',
             inputs=[in_prompt, in_optout],
             additional_inputs=[in_temperature, in_seed, in_bricks, in_rejections, in_regenerations],
-            outputs=[out_img, out_txt],
-            flagging_mode='never',
+            outputs=[out_img, out_txt, out_metadata],
+            flagging_options=[('Rate as Bad 😞', 'bad'),
+                              ('Rate as Okay 😐', 'okay'),
+                              ('Rate as Great 😄', 'great')],
+            flagging_dir=flagging_dir,
+            flagging_callback=self.logger,
             theme=gr.themes.Monochrome(),
         )
 
@@ -63,20 +71,39 @@ class Demo:
                 examples=[[name, example['prompt'], example['temperature'], example['seed'], example['output_img']]
                           for name, example in examples.items()],
                 inputs=[dummy_name, in_prompt, in_temperature, in_seed, dummy_out_img],
-                outputs=[out_img, out_txt],
-                fn=lambda *args: (args[-1], examples[args[0]]['output_txt']),
+                outputs=[out_img, out_txt, out_metadata],
+                fn=lambda *args: (args[-1], examples[args[0]]['output_txt'], examples[args[0]]),
                 run_on_click=True,
             )
 
-        self.demo.queue()
-
     def launch(self) -> None:
-        self.demo.launch()
+        self.demo.queue().launch()
+
+
+def get_help_string(field_name: str) -> str:
+    """
+    :param field_name: Name of a field in BrickGPTConfig.
+    :return: Help string for the field.
+    """
+    data_fields = fields(BrickGPTConfig)
+    name_field = next(f for f in data_fields if f.name == field_name)
+    return name_field.metadata['help']
+
+
+def get_examples(example_dir: str = str(Path(__file__).parent / 'examples')) -> dict[str, dict[str, str]]:
+    examples_file = os.path.join(example_dir, 'examples.json')
+    with open(examples_file) as f:
+        examples = json.load(f)
+
+    for example in examples.values():
+        example['output_img'] = os.path.join(example_dir, example['output_img'])
+    return examples
 
 
 class BrickGenerator:
-    def __init__(self, output_dir: str, model_cfg: BrickGPTConfig):
+    def __init__(self, output_dir: str, flagging_dir: str, model_cfg: BrickGPTConfig):
         self.output_dir = output_dir
+        self.flagging_dir = flagging_dir
         os.makedirs(output_dir, exist_ok=True)
         self.model_cfg = model_cfg
         self.model = None
@@ -101,7 +128,7 @@ class BrickGenerator:
             max_bricks: int | None,
             max_brick_rejections: int | None,
             max_regenerations: int | None,
-    ) -> tuple[str, str]:
+    ) -> tuple[str, str, dict[str, Any]]:
         self.model = BrickGPT(self.model_cfg)
 
         # Set model parameters
@@ -122,6 +149,7 @@ class BrickGenerator:
         with open(ldr_filename, 'w') as f:
             f.write(output['bricks'].to_ldr())
         generation_time = time.time() - start_time
+        output_txt = output['bricks'].to_txt()
         print(f'Finished generation in {generation_time:.1f}s!')
 
         # Render brick model to image
@@ -132,50 +160,67 @@ class BrickGenerator:
         rendering_time = time.time() - start_time - generation_time
         print(f'Finished rendering in {rendering_time:.1f}s!')
 
-        # Save data persistently
+        metadata = {
+            'uid': output_uuid,
+            'prompt': prompt,
+            'temperature': self.model.temperature,
+            'seed': seed,
+            'max_bricks': self.model.max_bricks,
+            'max_brick_rejections': self.model.max_brick_rejections,
+            'max_regenerations': self.model.max_regenerations,
+            'start_timestamp': time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(start_time)),
+            'generation_time': generation_time,
+            'rendering_time': rendering_time,
+            'output_txt': output_txt,
+        }
+
         if not do_not_save_data:
-            # Copy output image to persistent storage
-            img_copy_filename = os.path.join(self.save_data_dir, f'{output_uuid}.png')
-            shutil.copy(img_filename, img_copy_filename)
+            out_filename = os.path.join(self.flagging_dir, f'{metadata["uid"]}.json')
+            with open(out_filename, 'w') as f:
+                json.dump(metadata, f)
+            print(f'Saved data to {out_filename}.')
 
-            # Save metadata
-            metadata_filename = os.path.join(self.save_data_dir, f'{output_uuid}.json')
-            with open(metadata_filename, 'w') as f:
-                json.dump({
-                    'prompt': prompt,
-                    'temperature': self.model.temperature,
-                    'seed': seed,
-                    'max_bricks': self.model.max_bricks,
-                    'max_brick_rejections': self.model.max_brick_rejections,
-                    'max_regenerations': self.model.max_regenerations,
-                    'start_timestamp': time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(start_time)),
-                    'generation_time': generation_time,
-                    'rendering_time': rendering_time,
-                    'output_txt': output['bricks'].to_txt(),
-                }, f)
-            print(f'Saved data to {metadata_filename}.')
-
-        return img_filename, output['bricks'].to_txt()
+        return img_filename, output_txt, metadata
 
 
-def get_help_string(field_name: str) -> str:
-    """
-    :param field_name: Name of a field in BrickGPTConfig.
-    :return: Help string for the field.
-    """
-    data_fields = fields(BrickGPTConfig)
-    name_field = next(f for f in data_fields if f.name == field_name)
-    return name_field.metadata['help']
+class Logger(gr.FlaggingCallback):
+    def __init__(self):
+        self.components = None
+        self.flagging_dir = None
 
+    def setup(
+            self,
+            components: Sequence[Component],
+            flagging_dir: str | Path,
+    ):
+        self.components = components
+        self.flagging_dir = str(flagging_dir)
+        os.makedirs(flagging_dir, exist_ok=True)
 
-def get_examples(example_dir: str = str(Path(__file__).parent / 'examples')) -> dict[str, dict[str, str]]:
-    examples_file = os.path.join(example_dir, 'examples.json')
-    with open(examples_file) as f:
-        examples = json.load(f)
+    def flag(
+            self,
+            flag_data: list[Any],
+            flag_option: str | None = None,
+            username: str | None = None,
+    ) -> int:
+        components_and_data = zip(self.components, flag_data, strict=False)
+        metadata = next(data for component, data in components_and_data
+                        if component.label == 'out_metadata')
 
-    for example in examples.values():
-        example['output_img'] = os.path.join(example_dir, example['output_img'])
-    return examples
+        if metadata is None:
+            gr.Info('Can\'t flag an empty output.')
+            return 0
+        if metadata.get('example', False):
+            return 0  # Do not log example data
+
+        metadata['rating'] = flag_option
+
+        print(f'Logging flagged data: {metadata}')
+        out_filename = os.path.join(self.flagging_dir, f'{metadata["uid"]}.json')
+        with open(out_filename, 'w') as f:
+            json.dump(metadata, f)
+        print(f'Saved flagged data to {out_filename}.')
+        return 1
 
 
 with tempfile.TemporaryDirectory() as tmp_dir:
